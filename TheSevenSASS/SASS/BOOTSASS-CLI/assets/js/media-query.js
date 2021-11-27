bc7f8a32a7286e50b69d7d7e91255320f16e0d26
Code-Warlock <dessymichaeltgp123@gmail.com>
2021-11-27 21:15:10 +0100

    Added .gitignore

---
+++ TheSevenSASS/SASS/BOOTSASS-CLI/assets/js/media-query.js
@@ -0,0 +1,8 @@
+// let bigScreenNav = document.querySelector(".bigScreenNav");
+// let smallScreenNavCheck = window.matchMedia("max-width:400px");
+//
+// if (smallScreenNavCheck){
+//   bigScreenNav.parentNode.removeChild(bigScreenNav);
+// }else{
+
+// }
