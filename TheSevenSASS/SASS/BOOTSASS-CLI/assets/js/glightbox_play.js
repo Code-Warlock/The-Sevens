2162f45ba0b5c25c5f7adf3ec7c7309879feda53
Code-Warlock <dessymichaeltgp123@gmail.com>
2021-12-03 20:09:51 +0100

    Version 3.0 First stable release of HAYHUD-NIG

--- TheSevenSASS/SASS/BOOTSASS-CLI/assets/js/glightbox_play.js
+++ TheSevenSASS/SASS/BOOTSASS-CLI/assets/js/glightbox_play.js
@@ -9,42 +9,41 @@ const lightbox = GLightbox({
 const myGallery = GLightbox({
     elements: [
       {
-          'href': './assets/images/iru_soup.jpeg',
-          'type': 'image',
-          'description': 'Example',
-          'alt': 'image text alternatives'
+        'href': './assets/images/iru_process.jpeg',
+        'type': 'image',
+        'description': 'The process of manufacturing the locust bean spice',
+        'alt': 'gallery locust beans'
+      },
+      {
+        'href': './assets/images/iru_process2.jpeg',
+        'type': 'image',
+        'description': 'The process of manufacturing the locust bean spice',
+        'alt': 'gallery locust beans'
       },
       {
           'href': './assets/images/iru_soup.jpeg',
           'type': 'image',
-          'description': 'Example',
-          'alt': 'image text alternatives'
-      },
-      {
-          'href': './assets/images/iru_soup.jpeg',
-          'type': 'image',
-          'description': 'Example',
-          'alt': 'image text alternatives'
+          'description': 'An African soup dish prepared with locust beans',
+          'alt': 'gallery locust beans'
       },
       {
           'href': './assets/images/iru_soup2.jpeg',
           'type': 'image',
-          'description': 'Example',
-          'alt': 'image text alternatives'
+          'description': 'An African soup dish prepared with locust beans',
+          'alt': 'gallery locust beans'
       },
       {
-            'href': './assets/images/iru_soup3.jpeg',
-            'type': 'image',
-            'alt': 'image text alternatives'
-        },
-        {
-              'href': './assets/images/iru_soup4.jpeg',
-              'type': 'image',
-              'alt': 'image text alternatives'
-          },
-        {
-            'content': `<p>This will append some html inside the slide</p>`
-        },
+          'href': './assets/images/iru_soup3.jpeg',
+          'type': 'image',
+          'description': 'An African soup dish prepared with locust beans',
+          'alt': 'gallery locust beans'
+      },
+      {
+          'href': './assets/images/iru_soup4.jpeg',
+          'type': 'image',
+          'description': 'A full soup prepared with locust beans',
+          'alt': 'gallery locust beans'
+      },
     ],
     autoplayVideos: true,
 });
